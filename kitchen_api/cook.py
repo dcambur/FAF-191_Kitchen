70b36952ca5148033cee83ec1c97682d84336652
Dumitru Cambur <dcambur@gmail.com>
2021-10-14 18:31:35 +0300

    let the implementation begin

---
+++ kitchen_api/cook.py
@@ -0,0 +1,14 @@
+import threading, queue, itertools, time, requests, json, random
+import config
+
+class Cook(threading.Thread):
+    cook_id = itertools.count()
+
+    def __init__(self, q, tables=[], orders=[], loop_time = 1.0/60, *args, **kwargs):
+        self.q = q
+        self.timeout = loop_time
+        self.id = next(self.waiter_id)
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        pass
---
+++ kitchen_api/kitchen.py
@@ -0,0 +1,24 @@
+import threading, queue
+import config
+from cook import Cook
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/order', methods=['POST'])
+def distributor():
+    if request:
+        r = request.get_json(force=True)
+        print(r)
+        
+    return "Ok"
+
+
+if __name__ == "__main__":
+    # start order processor
+    processor_thread = threading.Thread(target=lambda: app.run(host='0.0.0.0', port='5000', debug=True, use_reloader=False))
+    processor_thread.start()
+
+    # start cooks
+    
+    processor_thread.join()
