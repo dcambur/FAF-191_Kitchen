75558a68b511d2324e3d57d5dfb6ae9f63354f5b
Dumitru Cambur <dcambur@gmail.com>
2021-11-02 23:34:02 +0200

    implementation updates

--- kitchen_api/config.py
+++ kitchen_api/config.py
@@ -4,6 +4,11 @@ COOKS = [{"name": "Bob Monkhouse",
           "rank": 1, 
           "proficiency": 1,
           "title": "Line Cook"},
+          {"name": "Robby Mouse", 
+          "catchphrase": "Bolts and Nuts!", 
+          "rank": 1, 
+          "proficiency": 1,
+          "title": "Line Cook"},
          {"name": "Dick Emery",
           "catchphrase": "Ooh, you are awful ... but I like you!",
           "rank": 2, 
@@ -11,11 +16,25 @@ COOKS = [{"name": "Bob Monkhouse",
           "title": "Saucier"},
          {"name": "Chris Tarrant",
           "catchphrase": "Is that your final answer?",
+          "rank": 2,
+          "proficiency": 2, 
+          "title": "Saucier"},
+          {"name": "Christy Wombat",
+          "catchphrase": "No catch.",
+          "rank": 3,
+          "proficiency": 3, 
+          "title": "Vice Executive Chef"},
+          {"name": "John Torry",
+          "catchphrase": "Your answer?",
           "rank": 3,
           "proficiency": 3, 
           "title": "Executive Chef"}]
 
-APPARATUSES = [{"type": "stove", "quantity": 2},
-              {"type": "oven", "quantity": 1}]
+APPARATUSES = [{"type": "stove", "quantity": 10},
+              {"type": "oven", "quantity": 10}]
 
+# docker
 DINING_HALL_URL = "http://dining-hall:5000/distribution"
+
+# direct run
+#DINING_HALL_URL = "http://localhost:5001/distribution"
--- kitchen_api/kitchen.py
+++ kitchen_api/kitchen.py
@@ -12,22 +12,22 @@ food_list = []
 apparatuses = []
 serve_lock = threading.Lock()
 apparatus_lock = threading.Lock()
-
+food_list_lock = threading.Lock()
 
 @app.route('/order', methods=['POST'])
 def processor():
     if request:
         r = request.get_json()
         print(f"New order received: {r}")
-        for item in r["items"]:
-            for menu_item in menu.menu:
-                if item == menu_item["id"]:
-                    food_lock = threading.Lock()
-                    food_list.append({"order_id": r["order_id"], "food": menu_item, "food_lock": food_lock, "prepared": False})
+        with food_list_lock:
+            for item in r["items"]:
+                for menu_item in menu.menu:
+                    if item == menu_item["id"]:
+                        food_lock = threading.Lock()
+                        food_list.append({"order_id": r["order_id"], "food": menu_item, "food_lock": food_lock, "prepared": False})
         order_lock = threading.Lock()
         with serve_lock:
             orders.append({"order": r, "lock": order_lock})
-        print(f"Orders in list: {len(orders)}, foods in food_list: {len(food_list)}")
         
     return "Ok"
 
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 
     # start cooks
     for cook_identity in config.COOKS:
-        cook = Cook(orders, food_list, serve_lock, apparatus_lock, identity=cook_identity, apparatuses=apparatuses)
+        cook = Cook(orders, food_list, food_list_lock, serve_lock, apparatus_lock, identity=cook_identity, apparatuses=apparatuses)
         cooks.append(cook)
         cook.start()
 
